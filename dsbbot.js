55712090c6a83b9005d697f8b5d393041801b749
TheWildBear <TheWildBear@users.noreply.github.com>
2017-11-24 21:33:16 +0100

    modularization of some parts
    move credentials from bot to file
    modularization of credentials

--- dsbbot.js
+++ dsbbot.js
@@ -1,17 +1,66 @@
 /*jshint esversion: 6 */
+console.log('Welcome!\nThis bot is a free software program, so you are allowed to redistribute it yourself! This program is from @thewildbear');
+const fs = require('fs');
+var token = '';
+var DSBusr = '';
+var DSBpwd = '';
 const util = require('util');
 const DSB = require('dsbapi');
 const http = require('http');
 const TeleBot = require('telebot');
-const bot = new TeleBot('your_token');
-const dsb = new DSB('username', 'password');
 const scrape = require('website-scraper');
 const untis = require("untis-api");
 const untis_entities = untis.entities;
-const fs = require('fs');
+const DomParser = require('dom-parser');
+const parser = new DomParser();
 const cheerio = require('cheerio');
+//variables
+var timetabletextsuffix = "The timetable for this week is: \n";
+var timetabletext = "";
+//suffix for the html site
+var suffix = 'suffix/to/yourclassestimetable.htm';
+var link;
+var valueofcell = "\t";
+var html = '';
+var row = 0;
+var counter = 0;
+var counter2 = 0;
+var first = 0;
+var pathtotimetable = '/path/to/your/time/table.htm';
+//load credentials from file
+fs.exists('./credentials', (exists) => {
+	if (exists) {
+		console.log('credentials file exists and is now loaded');
+		
+		fs.readFile('./credentials', 'utf8', function(err, credentialsdata){
+			if (err) throw err;
+			credentialsdata = credentialsdata.toString();
+			var credentials = credentialsdata.split(',');
+			token = credentials[0];
+			DSBusr = credentials[1];
+			DSBpwd = credentials[2];
+			DSBpwd = DSBpwd.replace(/(\n)/gm,"");
+		});
+	} else {
+		fs.open('./credentials', 'wx', (err, fd) => {
+			if (err) throw err;
+			console.error("The credentials file was not found, it's now created please fill it!");
+			fs.writeFile('./credentials', "Your_Telegram_Token,DSBusr,DSBpwd", function(err){
+				if(err)
+				{
+					return console.error(err);
+				}
+				process.exit(1);
+			})
+			
+	    });
+		
+	}
+});
 
-console.log("Bot started and api connected");
+setTimeout(() => {
+const bot = new TeleBot(token);
+const dsb = new DSB(DSBusr, DSBpwd);
 
 //start and connect bot
 bot.start();
@@ -20,36 +69,30 @@ bot.start();
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!\nThis bot is a free software program, so you are allowed to redistribute it yourself! This program is from @thewildbear'));
 
 bot.on([/dumpinfo*/, '/dumpinfo'] ,(msg) => {
-	let infofrommsg = util.inspect(msg, false, null);
-	msg.reply.text(infofrommsg);
+	msg.reply.text(util.inspect(msg, false, 99));
 });
 
 bot.on(['/gettimetable'], (msg) => {
 	gettimetable = function () {
 		data = dsb.fetch().then(data => {
-			const timetables = DSB.findMethodInData('timetable', data);
+			let timetables = DSB.findMethodInData('timetable', data);
 			let timetables2 = timetables.data[0].url;
-			console.log(util.inspect(timetables2,false,null));
 			//show link for debugging
-			let suffix = 'your/suffix/.htm'; // change this suffix for matching your timetable
-			//set suffix
-			let test = timetables2.substring(0, timetables2.indexOf('index.html')) + suffix;
+			let link = timetables2.substring(0, timetables2.indexOf('index.html')) + suffix;
 			//stitch the links together
-			console.log(test);
+			console.log("\nRequested timetable from: " + msg.from.id + " username: " +  msg.from.username + " at the time: " + Date() + "\n" + link + "\n");
 			//send link to dsbsite
-			bot.sendMessage(msg.from.id, test);
-						
-			//method through http get and cheerio 
+			bot.sendMessage(msg.from.id, "The current link to the timetable is: " + link + "\nat time: " + Date());
 			
 			//set options for page
 			var options = {
 					host: 'app.dsbcontrol.de',
 					port: 80,
-					path: '/link/to/htmfile/.htm'
+					path: pathtotimetable
 			};
 			
 			//var for html stuff
-			var html = '';
+			html = '';
 			
 			//download html and handle
 			http.get(options, function(res){
@@ -57,8 +100,40 @@ bot.on(['/gettimetable'], (msg) => {
 					html += data;
 				}).on('end', function(){
 					//do stuff with html code
-					console.log(html);
 					
+					$ = cheerio.load(html); //load html code into cheerio
+					//tbody
+					$('table').each(function(){
+						if(counter!==0)
+						{
+							valueofcell = $(this).text().trim();
+							valueofcell = valueofcell.replace(/(\r\n|\n|\r)/gm," ");
+							timetabletext = timetabletext + valueofcell + " ";
+						}
+
+						if(row<5){
+							if(counter2!==0){
+								counter++;
+								counter2++;
+								row++;
+								//cell++;
+							}else{
+								counter2++;
+							}
+						}else{
+							row=0;
+							//cell=0;
+							timetabletext = timetabletext + "\n";
+						}
+						
+					});
+					let timetabletextstiched = timetabletextsuffix + timetabletext;
+					msg.reply.text(timetabletextstiched, { parseMode: 'markdown' });
+					//reset vars
+					timetabletext = "";
+					counter = 0;
+					counter2 = 0;
+					row = 0;
 					
 				});
 			});
@@ -109,10 +184,9 @@ bot.on(['/substitution'], (msg) =>{
 		const newsubstitution = DSB.findMethodInData('timetable', data);
 		let substitutiontg;
 		
-    //specify directory for correct functioning
 		var options = {
  		urls: 'app.dsbcontrol.de',
-  		directory: '/link/to/htmfile/.htm'
+  		directory: '/link/to/your/table'
   		};
 		
 		// or with callback
@@ -139,3 +213,7 @@ bot.on(/(show\s)?kitty*/, (msg) => {
 bot.on(/(show\s)?cat*/, (msg) => {
     return msg.reply.photo('http://thecatapi.com/api/images/get');
 });
+
+
+
+}, 2000);
